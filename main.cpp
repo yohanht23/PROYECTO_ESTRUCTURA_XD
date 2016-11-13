7d424c9ba30f67afb813113b31530f79a43e6575
yohanht23 <yohanht23@gmail.com>
2016-11-13 12:59:15 -0400

    El Cosmos XD
    OLOLOLOLOLOL

--- main.cpp
+++ main.cpp
@@ -1,40 +1,45 @@
-/* ---------------
- * Final Project Year 2016
- * @Subject: Data Structure
- * @Author: Isocrates De La Cruz
- * @Enrollment: 2014-2287
- * ---------------- */
+/* -------------------------------
+ * Proyecto final 2016
+ * Estructura de Datos
+ * @Autor = Isocrates De La Cruz
+ * @Matricula = 2014-2287
+ --------------------------------- */
+
 #include <iostream>
 #include <stdlib.h>
+#include <array>
 
 using namespace std;
 
-/* ----------------
- * Global Variables
- * More easier to assign a value
- ------------------ */
-int resp;
+/*
+ * Clase Aerolinea
+ * Esta clase es el baseline
+ */
+class Aerolinea{
+    /*
+     * Variables
+     * Variables que usaremos mas adelante
+     */
+    public:
+        int resp;
+        char destinos[999][999];
+        char rutas[999][999];
+        int count = 0;
 
+    /*
+     * Constructor de la clase
+     * Obligatorio para empezar a correr el programa
+     * No contiene parametros
+     */
+    public:Aerolinea(){
 
-/* --------------
- * Class Program
-  --------------- */
-class Program{
-
-    /* -------------------
-     * Class Constructor
-     * Is Obligatory to Start the Program
-     * Take 0 Param
-     --------------------- */
-    public:Program(){
-        Menu();
     }
 
-    /* -------------------
-     * Menu method
-     * This is the menu that show the program
-     * This menu takes 0 params
-     --------------------- */
+    /*
+     * Metodo Menu
+     * Este es el menu que muestra el programa
+     * No contiene parametros
+     */
     public:void Menu(){
         system("cls");
         cout << "\t" << "-------- Aerolinea ITLA --------" << endl << endl;
@@ -59,11 +64,40 @@ class Program{
                 cout << "\t" << "Error al seleccionar una opcion!" << endl;
         }
     }
+
+    /*
+     * Metodo para Introducir los Destinos
+     * Aqui los destinos se guardaran en la variable: destinos
+     * No contiene paramentros
+     */
+    public:void IntroducirDestino(){
+
+    }
+
+    /*
+     * Metodo para introducir las rutas
+     * Aqui se crearan las rutas y se guardaran en la variable: rutas
+     * No contiene parametros
+     */
+    public:void IntroducirRuta(){
+
+    }
+
+    /*
+     * Metodo para buscar las rutas
+     * Aqui se mostraran las rutas que queramos buscar
+     * No contiene parametros
+     */
+    public:void BuscarRuta(){
+
+    }
 };
 
-
-
-
-int main() {
-    Program program;
+/*
+ * Metodo main
+ * Aqui empieza el programa
+ */
+int main(){
+    Aerolinea aerolinea;
 }
+
